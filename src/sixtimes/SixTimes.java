fbcf33e7977b9658ebbb9c33ff888335af657cf4
Drew Spencer <slugmandrew@gmail.com>
2011-05-13 09:36:05 +0100

    Testing 6times
    Signed-off-by: Drew Spencer <slugmandrew@gmail.com>

---
+++ src/sixtimes/SixTimes.java
@@ -0,0 +1,40 @@
+package sixtimes;
+
+public class SixTimes
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		
+		int j = 6;
+		int k = 0;
+		
+		for(int i = 1; i <= 12; i++)
+		{
+			k = i * j;
+			
+			System.out.println(i + " x " + j + " = " + k);
+		}
+		
+		
+	}
+	/*
+	 * Variables
+	 */
+
+	/*
+	 * Constructors
+	 */
+
+	/*
+	 * Custom Methods
+	 */
+
+	/*
+	 * Getters & Setters
+	 */
+}
